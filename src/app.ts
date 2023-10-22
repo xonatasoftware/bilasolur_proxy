8a15d77f16fb4d52c37348199b7ee363cbf839f9
Arnar Ingi Gunnarsson <arnaringigunnarsson@icloud.com>
2023-10-22 14:48:40 +0000

    upd

--- src/app.ts
+++ src/app.ts
@@ -8,5 +8,5 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
